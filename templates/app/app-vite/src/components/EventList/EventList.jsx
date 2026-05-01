9f96bcb0b15b0153edd31829cad51038cbaf0b5c
Abbas Rouhi <rouhi438@gmail.com>
2026-05-01 12:42:10 +0200

    fetch events and render list from API

--- templates/app/app-vite/src/components/EventList/EventList.jsx
+++ templates/app/app-vite/src/components/EventList/EventList.jsx
@@ -1,6 +1,7 @@
-import events from "../../data/events.js";
+//import events from "../../data/events.js";
 import EventCard from "../EventCard/EventCard.jsx";
 import { useOutletContext } from "react-router-dom";
+import { useState, useEffect } from "react";
 import "./EventList.css";
 
 // TODO: split each event below into its own EventCard component
@@ -9,7 +10,31 @@ import "./EventList.css";
 
 export default function EventList() {
   const { searchQuery } = useOutletContext();
-  const filteredEvents = events.filter((event) =>
+
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch("http://localhost:3001/events")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch events");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setEvents(data);
+      })
+      .catch((error) => setError(error.message))
+      .finally(() => setLoading(false));
+  }, []);
+  if (loading) return <p>Loading events...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  const filteredEvents = events?.filter((event) =>
     event.name.toLowerCase().includes(searchQuery.toLowerCase()),
   );
   return (
